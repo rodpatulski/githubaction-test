b1b50601f3d666fa6d0e301e980a9db5cfffe701
radek79 <radek79@gmail.com>
2024-01-18 13:23:55 -0800

    trying hot coffee api

--- src/action.js
+++ src/action.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 async function run() {
-  const response = await axios.get('https://futuramaapi.herokuapp.com/api/characters/dr-zoidberg/1');
+  const response = await axios.get('https://api.sampleapis.com/coffee/hot/1');
   const { data = [] } = response;
   const firstEntry = data[0];
   console.log(`${firstEntry.character}: ${firstEntry.quote}`);
